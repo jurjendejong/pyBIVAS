b069732ef7a7f0b5fff032abeb22effe403660e3
jurjendejong <jurjen.dejong@deltares.nl>
2022-06-09 12:54:12 +0200

    Version file

--- pyBIVAS/__init__.py
+++ pyBIVAS/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-"""Top-level package for Klimaatbestendige Netwerken."""
+"""Top-level package for pyBIVAS."""
 
 __author__ = """Jurjen de Jong"""
 __email__ = 'jurjen.dejong@deltares.nl'
-__version__ = '0.3.0'
+__version__ = '0.2.0'
